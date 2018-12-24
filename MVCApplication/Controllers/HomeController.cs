8157609213f1195d5bd7a8c834a9f25ae840cdc0
mystified131 <mystifiedthomas@gmail.com>
2018-12-24 07:39:18 -0600

    Inital commit.

---
+++ MVCApplication/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVCApplication.Models;
+using MVCApplication.ViewModels;
+
+namespace MVCApplication.Controllers
+{
+  
+    public class HomeController : Controller
+    {
+        static public List<string> TheList = new List<string>();
+
+        public IActionResult Index()
+        {
+            
+        IndexViewModel indexViewModel = new IndexViewModel();
+
+            return View(indexViewModel);
+        }
+
+        [HttpGet]
+        public IActionResult Result()
+        {
+            if(TheList.Count > 0) { 
+            ResultViewModel resultViewModel = new ResultViewModel();
+
+                resultViewModel.TheList = TheList;
+
+                return View(resultViewModel);
+            }
+
+            else
+            {
+                return Redirect("/");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Result(ResultViewModel resultViewModel)
+
+        {
+            if (ModelState.IsValid)
+            {
+
+                TheList.Add(resultViewModel.NewElement);
+
+                resultViewModel.TheList = TheList;
+
+                return View(resultViewModel);
+            }
+
+            return Redirect("/");
+
+        }
+
+    }
+
+
+
+}
---
+++ MVCApplication/Models/Listbuilder.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApplication.Models
+{
+
+    public class Listbuilder
+    {
+        public string Name { get; set; }
+        public Listbuilder(string name)
+        {
+            Name = name;
+
+        }
+
+
+    }
+
+}
+
+
