93d777ea1bcb0b41dcc0d6131454a863a188633f
mystified131 <mystifiedthomas@gmail.com>
2018-12-24 13:37:51 -0600

    Added Edit Functionality.

--- MVCApplication/Controllers/HomeController.cs
+++ MVCApplication/Controllers/HomeController.cs
@@ -9,15 +9,15 @@ using MVCApplication.ViewModels;
 
 namespace MVCApplication.Controllers
 {
-  
+
     public class HomeController : Controller
     {
         static public List<string> TheList = new List<string>();
 
         public IActionResult Index()
         {
-            
-        IndexViewModel indexViewModel = new IndexViewModel();
+
+            IndexViewModel indexViewModel = new IndexViewModel();
 
             return View(indexViewModel);
         }
@@ -25,8 +25,8 @@ namespace MVCApplication.Controllers
         [HttpGet]
         public IActionResult Result()
         {
-            if(TheList.Count > 0) { 
-            ResultViewModel resultViewModel = new ResultViewModel();
+            if (TheList.Count > 0) {
+                ResultViewModel resultViewModel = new ResultViewModel();
 
                 resultViewModel.TheList = TheList;
 
@@ -87,16 +87,85 @@ namespace MVCApplication.Controllers
 
                 removeViewModel.TheList = TheList;
 
-                return View(removeViewModel);
+                return Redirect("/Home/Result");
             }
 
             return Redirect("/");
 
         }
 
+        [HttpGet]
+        public IActionResult EditSelect()
+        {
+            if (TheList.Count > 0)
+            {
+                EditSelectViewModel editSelectViewModel = new EditSelectViewModel();
 
+                editSelectViewModel.TheList = TheList;
+
+                return View(editSelectViewModel);
+            }
+
+            else
+            {
+                return Redirect("/");
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult EditSelect(EditSelectViewModel editSelectViewModel)
+        {
+
+                if (ModelState.IsValid)
+                {
+
+                    ViewBag.NewElement1 = editSelectViewModel.NewElement1;
+                    TheList.Remove(editSelectViewModel.NewElement1);
+
+                return View("EditItem");
+                }
+
+                return Redirect("/");
+
+            }
+
+
+        [HttpGet]
+        public IActionResult EditItem()
+        {
+            if (TheList.Count > 0)
+            {
+                EditItemViewModel editItemViewModel = new EditItemViewModel();
+
+                return View(editItemViewModel);
+            }
+
+            else
+            {
+                return Redirect("/");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult EditItem(EditItemViewModel editItemViewModel)
+
+        {
+            if (ModelState.IsValid)
+
+            { 
+               
+
+                TheList.Add(editItemViewModel.NewElement2);
+
+                return Redirect("/Home/Result");
+            }
+
+            return Redirect("/");
+
+        }
     }
 
-
-
 }
+
+
